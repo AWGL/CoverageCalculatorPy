0804de27433ae7ef28975e4f2c9f9e1c9902b0cd
C W Medway <christopher.medway@nottingham.ac.uk>
2018-12-05 20:33:43 +0000

    added test for end of interval

--- tests.py
+++ tests.py
@@ -12,6 +12,7 @@ Updated: 11th November 2018
 import unittest
 import os
 from CoverageCalculatorPy import get_avg_depth
+from CoverageCalculatorPy import get_gaps
 
 class TestCalcCov(unittest.TestCase):
 
@@ -32,6 +33,23 @@ class TestCalcCov(unittest.TestCase):
                     self.assertEqual(avg_depth, 901.0)
 
 
+    def test_avg_depth_endofinterval(self):
+
+        if os.path.exists('./test.coverage'):
+            os.remove('./test.coverage')
+
+        filehandel = open("./test.coverage", 'a+')
+        depthfile = "test.gz"
+        get_avg_depth(filehandel, "1", 115258827, 115258834, "NONE", depthfile, 180)
+        filehandel.close()
+
+        with open("./test.coverage") as f:
+            for line in f:
+                if line.startswith("1\t115258827\t115258834"):
+                    avg_depth = float(line.split("\t")[4])
+                    self.assertEqual(avg_depth, 119.0)
+
+
     def test_perc_coverage(self):
 
         if os.path.exists('./test.coverage'):
@@ -48,6 +66,22 @@ class TestCalcCov(unittest.TestCase):
                     perc_coverage = float(line.split("\t")[5])
                     self.assertEqual(perc_coverage, 16.7)
 
+
+    def test_gaps(self):
+
+        if os.path.exists('./test.gaps'):
+            os.remove('./test.gaps')
+
+        filehandel = open("./test.gaps", 'a+')
+        depthfile = "test.gz"
+        get_gaps(filehandel, "1", 115252142, 115252148, "NONE", depthfile, 180)
+        filehandel.close()
+
+        with open("./test.gaps") as f:
+            for line in f:
+                if line.startswith("1\t115252142\t115252147"):
+
+                    self.assertEqual(line, "1\t115252142\t115252147\n")
 
 if __name__ == '__main__':
     unittest.main()
